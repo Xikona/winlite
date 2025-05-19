153d5881ed52046fb0ceb5da217424dc6a0f65ee
Xikona <m0109dm@yandex.ru>
2025-05-19 16:09:25 +0700

    Update user.js

--- Files/firefox/user.js
+++ Files/firefox/user.js
@@ -5,10 +5,5 @@ user_pref("svg.context-properties.content.enabled", true);
 user_pref("full-screen-api.transition-duration.enter", "0 0");
 user_pref("full-screen-api.transition-duration.leave", "0 0");
 user_pref("full-screen-api.warning.timeout", 0);
-// Disable sponsored content on Firefox Home & Clear default topsites
-user_pref("browser.newtabpage.activity-stream.showSponsored", false);
-user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
-user_pref("browser.newtabpage.activity-stream.default.sites", "");
-// homepage
-user_pref("browser.startup.page", 0);
-user_pref("browser.startup.homepage", "about:blank");
+// fix screen tearing
+user_pref("webgl.angle.force-d3d11", true);
